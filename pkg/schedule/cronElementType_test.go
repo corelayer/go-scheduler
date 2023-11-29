3df7960c73d9bccb411d72696168df383aa7a9c9
Jan Tytgat <jan.tytgat@corelayer.eu>
2023-11-29 11:11:52 +0100

    Test for CronElementType
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/schedule/cronElementType_test.go
+++ pkg/schedule/cronElementType_test.go
@@ -20,4 +20,18 @@ import "testing"
 
 // TODO TestCronElementType_String
 func TestCronElementType_String(t *testing.T) {
+	var (
+		result []string
+		wanted = []string{"second", "minute", "hour", "day", "month", "weekday", "year"}
+	)
+
+	for i := 0; i < len(wanted); i++ {
+		result = append(result, CronElementType(i).String())
+	}
+
+	for j := 0; j < len(wanted); j++ {
+		if result[j] != wanted[j] {
+			t.Errorf("invalid string: got %s expected %s", result[j], wanted[j])
+		}
+	}
 }
